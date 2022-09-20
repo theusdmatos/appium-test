eca1e5a826ba98b96199b2298eb46e3ee4683271
theusdmatos <theusdmatos@gmail.com>
2022-09-20 00:13:20 -0300

    Tests

---
+++ session.py
@@ -0,0 +1,7 @@
+import unittest
+from appium import webdriver
+
+desired_caps = {}
+desired_caps['app'] = "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App"
+desired_caps['platformName'] = "Windows"
+driver = webdriver.Remote('http://127.0.0.1:4723', desired_caps)
